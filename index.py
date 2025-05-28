a190f31433b3afd260e862701a15a3fd9795cab8
ilhamnrfdn <ilhamnoer@femaledaily.com>
2025-05-28 10:21:43 +0700

    first commit

---
+++ index.py
@@ -0,0 +1,16 @@
+x= 10;
+
+# defie function
+def my_function():
+    y = 20;
+    return x + y;
+
+result = my_function();
+print(result)  # Output: 30
+
+a = [1,2,3];
+a[2] = 4;  # modify the list
+
+print(a)
+
+
